9cac28a48328157f85666df57c75d9ee6e4e7fcb
Sean Williams <SWilliams17655@hotmail.com>
2024-02-22 20:31:50 -0600

    Updated user interface allowing user to control training and added feature to save trained models.

--- agent.py
+++ agent.py
@@ -1,10 +1,9 @@
 import random
-
 import tensorflow as tf
 import numpy as np
 from keras.optimizers import RMSprop
+from keras.models import load_model
 from collections import deque
-
 
 class Agent:
     def __init__(self):
@@ -17,12 +16,9 @@ class Agent:
         self.train_set_size = 1000
         self.memory = deque(maxlen=self.train_set_size)
         self.model = self.build_model()
-
         self.batch_size = 124
         self.num_epoch = 30
 
-    # *********************************************************************************************************************
-    # *
     # *********************************************************************************************************************
     def build_model(self):
         num_hidden_layer_1 = 512
@@ -44,14 +40,10 @@ class Agent:
         return model
 
     # *********************************************************************************************************************
-    # *
-    # *********************************************************************************************************************
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    # *********************************************************************************************************************
-    # *
     # *********************************************************************************************************************
 
     def get_action(self, state):
@@ -59,53 +51,61 @@ class Agent:
             action = np.random.randint(0, self.action_space)
 
         else:
-            # Add an extra dimension to the state to create a batch with one instance. TensorFlow NN
-            # requires inputs be in batches, but can be confusing when we have only one input.
+            # Add an extra dimension to the state to create a batch with one instance.
             state = np.expand_dims(state, axis=0)
 
-            # Use the model to predict the Q-values (action values) for the given state
+            # Use the model to predict the Q-values for the given state
             q_values = self.model.predict(state, verbose=0)
 
-            # Select and return the action with the highest Q-value
             action = np.argmax(q_values[0])  # Take the action from the first (and only) entry
 
         return int(action)
 
     # *********************************************************************************************************************
-    # *
-    # *********************************************************************************************************************
 
     def learn(self):
-        if len(self.memory) < self.train_set_size:
+        if len(self.memory) < self.train_set_size:  # If there are not enough training cases returns.
             return
 
-        training_set = random.sample(self.memory, int(len(self.memory) * 0.5))
+        training_set = random.sample(self.memory, int(len(self.memory) * 0.5)) # Randomly samples half
 
         state = np.zeros((len(training_set), self.state_size))
         next_state = np.zeros((len(training_set), self.state_size))
         action, reward, done = [], [], []
 
-        for i in range(len(training_set)):
+        for i in range(len(training_set)):  # Splits up memory to create training set
             state[i] = training_set[i][0]
             action.append(training_set[i][1])
             reward.append(training_set[i][2])
             next_state[i] = training_set[i][3]
             done.append(training_set[i][4])
 
-        target = self.model.predict(state)
-        target_next = self.model.predict(next_state)
+        target = self.model.predict(state) # Makes prediction for current state
+        target_next = self.model.predict(next_state) # Makes prediction for next state
 
-        # correction on the Q value for the action used
         for i in range(len(training_set)):
             if done[i]:
-                target[i][action[i]] = reward[i]
+                target[i][action[i]] = reward[i] # If done then updates Q-Values with reward
             else:
+                # Back propagates reward then reduces value by Gamma
                 target[i][action[i]] = reward[i] + self.gamma * (np.amax(target_next[i]))
 
+        # Decreases epsilon to ensure less random decisons are made after each learning iteration
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
+        # Resets learning memory to load new training cases.
         self.memory = deque(maxlen=self.train_set_size)
-        self.model.fit(state, target, batch_size=self.batch_size, epochs=self.num_epoch, verbose=0)
 
+        # Fits model based on
+        self.model.fit(state, target, batch_size=self.batch_size, epochs=self.num_epoch, verbose=2)
 
+    # *********************************************************************************************************************
+
+    def load(self, name):
+        self.model = load_model(name)
+
+    # *********************************************************************************************************************
+
+    def save(self, name):
+        self.model.save(name)
--- main.py
+++ main.py
@@ -5,49 +5,63 @@ import matplotlib.pyplot as plt
 
 env = gym.make("CartPole-v1")
 
-debug = False
-num_episodes = 500
-max_steps = 500
-score = []
+debug = False # Set to true to log debug features.
+num_episodes = 300
+max_steps = 300
+score = [] # Saves score during each iteration for graphing.
 agent = Agent()
 
-for e in range(num_episodes):
-    state, _ = env.reset()
-    done = False
-    step = 0
+answer = input("Would you like to load an agent? [y/n]")
+if answer == 'y' or answer == 'Y':
+    print("Loading agent")
+    agent.load("agent.h5")
+else:
+    print("Starting with untrained agent")
 
-    while not done and step < max_steps:
-        action = agent.get_action(state)
-        new_state, reward, done, _, _ = env.step(action)
-        if not done or step >= max_steps - 1:
-            reward = reward
-        else:
-            reward = -100
+answer = input("Would you like to train network? [y/n]")
+if answer == 'y' or answer == 'Y':
+    print("Beginning training process")
+    for e in range(num_episodes):
+        state, _ = env.reset()
+        done = False
+        step = 0
 
-        if debug:
-            print(f"Step: {step}, State: {state}, Action: {action}, Reward: {reward}, New State: {new_state}, Done: {done}")
+        while not done and step < max_steps:
+            action = agent.get_action(state)
+            new_state, reward, done, _, _ = env.step(action)
+            if not done or step >= max_steps - 1: # If the pole falls before max_sets sets to -100
+                reward = reward
+            else:
+                reward = -100
 
-        agent.remember(state, action, reward, new_state, done)
-        state = new_state
-        step += 1
-        agent.learn()
+            if debug:
+                print(f"Step: {step}, State: {state}, Action: {action}, Reward: {reward}, New State: {new_state}, Done: {done}")
 
-    print(f"Episode: {e}, Number Steps: {step}, Epsilon: {agent.epsilon}")
-    score.append(step)
+            agent.remember(state, action, reward, new_state, done)  # Saves information in memory for training.
+            state = new_state
+            step += 1
+            agent.learn()  # Implements learning algorithm.
 
-sns.set_style("darkgrid", {"axes.facecolor": ".9"})
-sns.lineplot(data=score)
-plt.xlabel("Training Episode")
-plt.ylabel("Number of Steps Achieved")
-plt.title("Deep Quality Network Learning Score")
-plt.show()
+        print(f"Episode: {e}, Number Steps: {step}, Epsilon: {agent.epsilon}")
+        score.append(step)
+
+    agent.save("agent.h5")
+
+    #  Graphs the learning progress
+    sns.set_style("darkgrid", {"axes.facecolor": ".9"})
+    sns.lineplot(data=score)
+    plt.xlabel("Training Episode")
+    plt.ylabel("Number of Steps Achieved")
+    plt.title("Deep Quality Network Learning Score")
+    plt.show()
+
 env = gym.make("CartPole-v1", render_mode="human")
 
 for e in range(num_episodes):
     state, _ = env.reset()
     done = False
     step = 0
-
+    agent.epsilon = agent.epsilon_min  # Setting epsilon to min to demonstrate full learning
     while not done and step < max_steps:
         action = agent.get_action(state)
         new_state, reward, done, _, _ = env.step(action)
@@ -56,10 +70,5 @@ for e in range(num_episodes):
         else:
             reward = -100
 
-        if debug:
-            print(f"Step: {step}, State: {state}, Action: {action}, Reward: {reward}, New State: {new_state}, Done: {done}")
-
-        agent.remember(state, action, reward, new_state, done)
         state = new_state
         step += 1
-    print(f"Episode: {e}, Number Steps: {step}, Epsilon: {agent.epsilon}")
