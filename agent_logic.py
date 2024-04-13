aef599cde3a0a8174622a7daa3a0f2df78ae9ddd
Sean Williams <SWilliams17655@hotmail.com>
2024-04-12 23:22:24 -0500

    Updated forward pass to compute all neurons using a single matrix to improve processing time.

--- agent_logic.py
+++ agent_logic.py
@@ -1,45 +1,40 @@
 import random
 import tensorflow as tf
 import numpy as np
-import math
 from keras.models import load_model
 from collections import deque
 
 
 class Agent_Logic:
-    def __init__(self):
+    def __init__(self, input_array_size, epsilon):
+        print(f"\nThe following GPU processor is being used: {tf.config.list_physical_devices('GPU')}")
 
         self.learning_rate = 0.001
         self.learning_rate_decay = .98
-        self.state_size = 38
+        self.state_size = input_array_size
         self.action_space = 8
         self.gamma = 0.98
-        self.epsilon = 1.0
-        self.epsilon_decay = .90
-        self.epsilon_min = 0.08
-        self.train_set_size = 1000
-        self.memory = deque(maxlen=self.train_set_size)
+        self.epsilon = epsilon
+        self.epsilon_decay = .95
+        self.epsilon_min = 0.1
+        self.train_set_size = int(10000 - 10000 * self.epsilon)
+        self.memory = deque(maxlen=10000)
         self.model = self.build_model()
         self.batch_size = 64
         self.num_epoch = 30
 
     # *********************************************************************************************************************
     def build_model(self):
-        num_hidden_layer_1 = 10
-        num_hidden_layer_2 = 20
-        num_hidden_layer_3 = 10
 
         # Create a sequential model
-        model = tf.keras.Sequential([
-            tf.keras.layers.Dense(num_hidden_layer_1, input_shape=(self.state_size,), kernel_initializer='he_uniform',
-                                  activation='relu'),
-            tf.keras.layers.Dense(num_hidden_layer_2, kernel_initializer='he_uniform', activation='relu'),
-            tf.keras.layers.Dense(num_hidden_layer_3, kernel_initializer='he_uniform', activation='relu'),
-            tf.keras.layers.Dense(self.action_space, activation='linear', kernel_initializer='he_uniform')
-        ])
+        model = tf.keras.Sequential()
+        model.add(tf.keras.Input(shape = (self.state_size,)))
+        model.add(tf.keras.layers.Dense(128, activation='relu', kernel_initializer='he_uniform'))
+        model.add(tf.keras.layers.Dense(64, activation='relu', kernel_initializer='he_uniform'))
+        model.add(tf.keras.layers.Dense(32, activation='relu', kernel_initializer='he_uniform'))
+        model.add(tf.keras.layers.Dense(self.action_space, activation='linear', kernel_initializer='he_uniform'))
 
-        model.compile(loss="mse", optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate),
-                      metrics=["accuracy"])
+        model.compile(loss="mse", optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate), metrics=["mean_absolute_error"])
 
         model.summary()
 
@@ -48,26 +43,14 @@ class Agent_Logic:
     # *********************************************************************************************************************
 
     def remember(self, state, action, reward, next_state):
-        # print(f"{state}, {action}, {reward}, {next_state}", flush=True )
         self.memory.append((state, action, reward, next_state))
 
     # *********************************************************************************************************************
+    # Use the model to predict the Q-values for the given state
+    def get_q_values(self, states):
 
-    def get_action(self, state):
-
-        if np.random.rand() <= self.epsilon:
-            action = np.random.randint(0, self.action_space)
-
-        else:
-            # Add an extra dimension to the state to create a batch with one instance.
-            state = np.expand_dims(state, axis=0)
-
-            # Use the model to predict the Q-values for the given state
-            q_values = self.model.predict(state, verbose=0)
-
-            action = np.argmax(q_values[0])  # Take the action from the first (and only) entry
-
-        return int(action)
+        q_values = self.model.predict(states, verbose=0)
+        return q_values
 
     # *********************************************************************************************************************
 
@@ -75,7 +58,7 @@ class Agent_Logic:
         if len(self.memory) < self.train_set_size:  # If there are not enough training cases returns.
             return
 
-        training_set = random.sample(self.memory, int(len(self.memory) * 0.5))  # Randomly samples half
+        training_set = random.sample(self.memory, int(len(self.memory) * 0.75))  # Randomly samples half
 
         state = np.zeros((len(training_set), self.state_size))
         next_state = np.zeros((len(training_set), self.state_size))
@@ -93,17 +76,21 @@ class Agent_Logic:
         for i in range(len(training_set)):
             target[i][action[i]] = reward[i] + self.gamma * (np.argmax(target_next[i]))
 
-        # Decreases epsilon to ensure less random decisons are made after each learning iteration
+        # Decreases epsilon to ensure less random decisions are made after each learning iteration
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-            print(f"Updated Epsilon: {self.epsilon}")
+            self.train_set_size = int(10000 - 10000 * self.epsilon)
+            print(f"Updated Epsilon is {self.epsilon} & training set size is {self.train_set_size}")
+
 
         # Resets learning memory to load new training cases.
         self.memory = deque(maxlen=self.train_set_size)
 
+        print()
+
         # Fits model based on
         self.model.fit(state, target, batch_size=self.batch_size, epochs=self.num_epoch, verbose=1)
-        self.save("last_save.h5")
+        self.save("last_save.keras")
     # *********************************************************************************************************************
 
     def load(self, name):
--- main.py
+++ main.py
@@ -1,26 +1,38 @@
 from environment import Environment
-from multiprocessing import Process
-import os
+
 # ********************************
 # Loading the environment
 # ********************************
 valid_option = False
+load_new_terrain = False
+load_prior_agent = False
 env = []
 
-# while not valid_option:
-#     option_selected = input("Select an option for your environment:\n"
-#                             "1: Create a new terrain map.\n"
-#                             "2: Load the previous terrain map.\n")
-#
-#     if option_selected == '1':
-#         valid_option = True
-#
-#     elif option_selected == '2':
-#         valid_option = True
-#
-#     else:
-#         print("Invalid selection")
+while not valid_option:
+    response = input("Create a new terrain map [y/n]: \n")
+    if response == 'y':
+        load_new_terrain = True
+        valid_option = True
+    elif response == 'n':
+        load_new_terrain = False
+        valid_option = True
+    else:
+        print("Invalid selection")
+
+valid_option = False
+
+while not valid_option:
+    response = input("Load prior trained agent [y/n]: \n")
+    if response == 'y':
+        load_prior_agent = True
+        valid_option = True
+    elif response == 'n':
+        load_new_agent = False
+        valid_option = True
+    else:
+        print("Invalid selection")
+
 
 if __name__ == "__main__":
-    env = Environment(load_new_terrain=False)
+    env = Environment(load_new_terrain=load_new_terrain, load_prior_agent=load_prior_agent, epsilon=.75)
     env.show_display()
